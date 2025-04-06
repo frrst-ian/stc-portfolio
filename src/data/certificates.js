98dc1018a3221eb0f31a2a7c552470f446fa51e9
frrst-ian <frrstxd@proton.me>
2025-04-06 19:40:20 +0800

    feat: add new certificates data

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,4 +1,6 @@
 import cyberSecImg from '../assets/images/cybersecurity.png';
+import dataVizImg from '../assets/images/data-viz.png';
+
 export const certificates = [
     {
         id: 1,
@@ -11,13 +13,13 @@ export const certificates = [
         imageAlt: 'Cisco Cybersecurity Certificate'
     },
     {
-        id:2 ,
+        id: 2,
         title: 'Data Visualization',
-        issuer: 'Cisco Networking Academy',
-        category: 'Cybersecurity',
-        desc: 'This course  introduced me inside the world of cybersecurity.I learned cybersecurity basics to protect my personal digital life and gain insights into the biggest security challenges companies, governments, and educational institutions face today. ',
-        link: 'https://www.netacad.com/courses/introduction-to-cybersecurity?courseLang=en-US',
-        imageUrl: cyberSecImg,
-        imageAlt: 'Cisco Cybersecurity Certificate'
+        issuer: 'freeCodeCamp',
+        category: 'Data Visualization',
+        desc: 'In this course, I learned about data visualization using D3.js. I also grasp the basics of AJAX and JSON, this tools enables me to work with APIs, which helped me to get my essential data. ',
+        link: 'https://www.freecodecamp.org/learn/data-visualization/',
+        imageUrl: dataVizImg,
+        imageAlt: 'freeCodeCamp Data Visualization Certificate'
     },
 ];
