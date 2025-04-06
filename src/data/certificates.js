8157cd2674ac4ee0c37df05b24b4022b8dc1de7f
frrst-ian <frrstxd@proton.me>
2025-04-06 19:33:57 +0800

    feat: add new certificates data

---
+++ src/data/certificates.js
@@ -0,0 +1,23 @@
+import cyberSecImg from '../assets/images/cybersecurity.png';
+export const certificates = [
+    {
+        id: 1,
+        title: 'Introduction to Cybersecurity',
+        issuer: 'Cisco Networking Academy',
+        category: 'Cybersecurity',
+        desc: 'This course  introduced me inside the world of cybersecurity.I learned cybersecurity basics to protect my personal digital life and gain insights into the biggest security challenges companies, governments, and educational institutions face today. ',
+        link: 'https://www.netacad.com/courses/introduction-to-cybersecurity?courseLang=en-US',
+        imageUrl: cyberSecImg,
+        imageAlt: 'Cisco Cybersecurity Certificate'
+    },
+    {
+        id:2 ,
+        title: 'Data Visualization',
+        issuer: 'Cisco Networking Academy',
+        category: 'Cybersecurity',
+        desc: 'This course  introduced me inside the world of cybersecurity.I learned cybersecurity basics to protect my personal digital life and gain insights into the biggest security challenges companies, governments, and educational institutions face today. ',
+        link: 'https://www.netacad.com/courses/introduction-to-cybersecurity?courseLang=en-US',
+        imageUrl: cyberSecImg,
+        imageAlt: 'Cisco Cybersecurity Certificate'
+    },
+];
