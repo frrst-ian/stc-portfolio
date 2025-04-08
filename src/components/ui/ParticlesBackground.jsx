d64b99334a8ec937843432f3b2f1543c487bbd13
frrst-ian <frrstxd@proton.me>
2025-04-08 23:33:11 +0800

    style: enhance the style of the homepage

--- src/components/ui/ParticlesBackground.jsx
+++ src/components/ui/ParticlesBackground.jsx
@@ -1,89 +1,132 @@
-import React, { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useMemo, useState } from "react";
+// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
-export const ParticlesBackground = () => {
-  const particlesInit = useCallback(async (engine) => {
-    await loadFull(engine);
+
+
+const ParticlesComponent = (props) => {
+
+  const [init, setInit] = useState(false);
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
-  return (
-    <Particles
-      id="tsparticles"
-      init={particlesInit}
-      options={{
-        fullScreen: {
-          enable: false
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+
+  const options = useMemo(
+    () => ({
+      fullScreen: {
+        enable: false, // ←- this is crucial
+        zIndex: 0
+      },
+      background: {
+        color: {
+          value: "#06040b",
         },
-        style: {
-          position: "absolute",
-          height: "500px",
-          width: "100%",
-          zIndex: -1
-        },
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "repulse",
           },
-          modes: {
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 100,
-              duration: 0.4,
-            },
+          onHover: {
+            enable: true,
+            mode: 'grab',
           },
         },
-        particles: {
-          color: {
-            value: "#ab91e8",
+        modes: {
+          push: {
+            distance: 200,
+            duration: 15,
           },
-          links: {
-            color: "#520bfe",
+          grab: {
             distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-              default: "bounce",
-            },
-            random: false,
-            speed: 2,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            value: { min: 1, max: 5 },
           },
         },
-        detectRetina: true,
-      }}
-    />
+      },
+      particles: {
+        color: {
+          value: "#eeebf5",
+        },
+        links: {
+          color: "#eeebf5",
+          distance: 150,
+          enable: true,
+          opacity: 0.3,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: true,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 150,
+        },
+        opacity: {
+          value: 1.0,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 3 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+
+  return (
+    <div style={{ 
+      height: '760px',
+      width: '100%',
+      position: 'absolute',
+      overflow: 'hidden',
+      zIndex: -1,
+    }}>
+      {init && <Particles
+        id={props.id}
+        init={particlesLoaded}
+        options={options}
+        canvasClassName="particles-canvas"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+        }}
+      />}
+    </div>
   );
 };
+
+export default ParticlesComponent;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,68 @@
 import React from "react";
 import "./HomePage.css";
-import { ParticlesBackground } from "../components/ui/ParticlesBackground";
+import ParticlesComponent from "../components/ui/ParticlesBackground";
 
 export const HomePage = () => {
   return (
     <main className="home">
-      <ParticlesBackground />
+      <ParticlesComponent id="particles" />
       <section className="top">
-        <h1 className='main-text'>Special Topics in Computing Portfolio</h1>
+        <h1 className="main-text">Special Topics in Computing Portfolio</h1>
+        <p>A curated list of my certificates from courses I finished. </p>
+
+        <div className="btn-container">
+          <button className="btn primary cert"><span>My certificates</span></button>
+          <button className="btn primary github"><span>View on github</span></button>
+        </div>
+
+        <div className="cards">
+          <div className="card">
+            <p>
+              10 <br />earned certificates
+            </p>
+          </div>
+          <div className="card">
+            28+ <br /> hours of effort
+          </div>
+        </div>
       </section>
+
+      <section className="middle">
+        <h2>Top Certificates</h2>
+        <div className="top-certs-wrapper">
+          <div className="top-certs">
+            <img
+              src="src/assets/images/cybersecurity.png"
+              alt="cisco cybersecurity certificate"
+            />
+          </div>
+          <div className="top-certs">
+            <img
+              src="src/assets/images/data-viz.png"
+              alt="freeCodeCamp data visualization certificate"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="last">
+        <div className="company-cards">
+          <div className="company">Cisco</div>
+          <div className="company">Amazon</div>
+          <div className="company">Matlab</div>
+          <div className="company">freeCodeCamp</div>
+        </div>
+        <h3>Issued by different companies</h3>
+      </section>
+
+      <footer>
+        <div>
+          <p>Copyright © 2025 Ian Forrest Rogel</p>
+        </div>
+        <div className="socials">
+          <div className="socials-icon"></div>
+        </div>
+      </footer>
     </main>
   );
 };
