6d84ebfe07fef83d1da6ffc9aaf574f2cace9a6e
frrst-ian <frrstxd@proton.me>
2025-04-09 13:28:32 +0800

    style: enhance styling of the about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./AboutPage.css";
 
 export const AboutPage = () => {
-    return (
-        <main>
-            <p>About</p>
-        </main>
-    )
-}
+  return (
+    <main className="about">
+      <h1>About</h1>
+      <div className="text-wrapper">
+          <p className="about-text">
+              Hi and welcome! I'm Ian, and I'm thrilled to have you here. This website
+              showcases a carefully curated collection of certificates from the online
+              courses I've explored, diving into some of the most exciting and emerging
+              technologies of our time. For more insights and details, make sure to
+              visit the Certificates Page. Bon voyage!
+          </p>
+      </div>
+    </main>
+  );
+};
